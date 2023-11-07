7093251256d10595d629dd8342e56a02a9b01ca5
Konsta <konsta09@gmail.com>
2023-11-07 22:41:35 +0200

    drm/vc4: fix build with clang
    drivers/gpu/drm/vc4/vc4_kms.c:372:3: error: variable 'mux' is used uninitialized whenever switch default is taken [-Werror,-Wsometimes-uninitialized]
                    default:
                    ^~~~~~~
    
    drivers/gpu/drm/vc4/vc4_hvs.c:2035:20: error: variable 'base' is uninitialized when used here [-Werror,-Wuninitialized]
                              VC4_SET_FIELD(base, SCALER6_DISPX_COB_BASE));
                                            ^~~~

--- drivers/gpu/drm/vc4/vc4_hvs.c
+++ drivers/gpu/drm/vc4/vc4_hvs.c
@@ -1971,6 +1971,7 @@ static int vc4_hvs_cob_init(struct vc4_hvs *hvs)
 		#define VC6_COB_NUM_LINES	4
 		base = 0;
 		top = 3840;
+		base = 0;
 
 		HVS_WRITE(SCALER6_DISPX_COB(2),
 			  VC4_SET_FIELD(top, SCALER6_DISPX_COB_TOP) |
