a691e40488929bef263cd393cfe4c4aa8e52807e
Konsta <konsta09@gmail.com>
2025-05-02 13:10:00 +0300

    Revert "PCI: brcmstb: Use internal register to change link capability"
    This reverts commit 8b2853fd6ef802ee6c4614d69b2467a2354e3ac1.

--- drivers/pci/controller/pcie-brcmstb.c
+++ drivers/pci/controller/pcie-brcmstb.c
@@ -500,10 +500,10 @@ static void brcm_pcie_munge_pll(struct brcm_pcie *pcie)
 static void brcm_pcie_set_gen(struct brcm_pcie *pcie, int gen)
 {
 	u16 lnkctl2 = readw(pcie->base + BRCM_PCIE_CAP_REGS + PCI_EXP_LNKCTL2);
-	u32 lnkcap = readl(pcie->base + PCIE_RC_CFG_PRIV1_LINK_CAPABILITY);
+	u32 lnkcap = readl(pcie->base + BRCM_PCIE_CAP_REGS + PCI_EXP_LNKCAP);
 
 	lnkcap = (lnkcap & ~PCI_EXP_LNKCAP_SLS) | gen;
-	writel(lnkcap, pcie->base + PCIE_RC_CFG_PRIV1_LINK_CAPABILITY);
+	writel(lnkcap, pcie->base + BRCM_PCIE_CAP_REGS + PCI_EXP_LNKCAP);
 
 	lnkctl2 = (lnkctl2 & ~0xf) | gen;
 	writew(lnkctl2, pcie->base + BRCM_PCIE_CAP_REGS + PCI_EXP_LNKCTL2);
