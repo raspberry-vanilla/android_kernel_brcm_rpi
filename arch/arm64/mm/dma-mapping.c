3d1cca2d9b2ab4a020ec790a4c5c21ebc79a643b
Konsta <konsta09@gmail.com>
2026-05-08 13:05:21 +0300

    Revert "arm64/dma-mapping: Fix arch_sync_dma_for_device to respect dir parameter"
    This reverts commit dc3cfb9a8d4243206c80a33740022c943325f9a9.

--- arch/arm64/mm/dma-mapping.c
+++ arch/arm64/mm/dma-mapping.c
@@ -17,22 +17,8 @@ void arch_sync_dma_for_device(phys_addr_t paddr, size_t size,
 			      enum dma_data_direction dir)
 {
 	unsigned long start = (unsigned long)phys_to_virt(paddr);
-	unsigned long end = start + size;
 
-	switch (dir) {
-	case DMA_BIDIRECTIONAL:
-		dcache_clean_inval_poc(start, end);
-		break;
-	case DMA_TO_DEVICE:
-		dcache_clean_poc(start, end);
-		break;
-	case DMA_FROM_DEVICE:
-		dcache_inval_poc(start, end);
-		break;
-	case DMA_NONE:
-	default:
-		break;
-	}
+	dcache_clean_poc(start, start + size);
 }
 
 void arch_sync_dma_for_cpu(phys_addr_t paddr, size_t size,
